78887b1ec561341f1e58ce2fa72c4e2a88dddae5
Tony Lea <tonylea@deadrobot.co.uk>
2018-10-13 10:27:55 +0100

    Display order state with JSX

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -31,22 +31,18 @@ class App extends Component {
   };
 
   render() {
+    const { fishes, order } = this.state;
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => (
-              <Fish
-                key={key}
-                index={key}
-                details={this.state.fishes[key]}
-                addToOrder={this.addToOrder}
-              />
+            {Object.keys(fishes).map(key => (
+              <Fish key={key} index={key} details={fishes[key]} addToOrder={this.addToOrder} />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={fishes} order={order} />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
